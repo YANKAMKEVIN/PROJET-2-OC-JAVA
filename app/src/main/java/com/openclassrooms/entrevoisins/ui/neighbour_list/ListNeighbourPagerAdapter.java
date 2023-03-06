4be3920007ae4e53aa3777626eba62d8cff0627d
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-06 21:37:47 +0100

    commit UI classes

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+
+public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
+
+    public ListNeighbourPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * getItem is called to instantiate the fragment for the given page.
+     *
+     * @param position
+     * @return
+     */
+    @Override
+    public Fragment getItem(int position) {
+        Boolean isFavourite = (position != 0);
+        return NeighbourFragment.newInstance(isFavourite);
+    }
+
+    /**
+     * get the number of pages
+     *
+     * @return
+     */
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
@@ -0,0 +1,92 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.events.DeleteNeighbourEvent;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeighbourRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Neighbour> mNeighbours;
+    private Context mContext;
+
+    public MyNeighbourRecyclerViewAdapter(List<Neighbour> items, Context context) {
+        mNeighbours = items;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_neighbour, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Neighbour neighbour = mNeighbours.get(position);
+        holder.mNeighbourName.setText(neighbour.getName());
+        Glide.with(holder.mNeighbourAvatar.getContext())
+                .load(neighbour.getAvatarUrl())
+                .apply(RequestOptions.circleCropTransform())
+                .into(holder.mNeighbourAvatar);
+
+        holder.mDeleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().post(new DeleteNeighbourEvent(neighbour));
+            }
+        });
+        holder.parentLayout.setOnClickListener(v -> {
+            Intent intent = new Intent(mContext, DetailsNeighbourActivity.class);
+            intent.putExtra("neighbourId",neighbour.getId());
+            intent.putExtra("neighbourName",neighbour.getName());
+            intent.putExtra("neighbourAvatarUrl",neighbour.getAvatarUrl());
+            intent.putExtra("neighbourAboutMe",neighbour.getAboutMe());
+            intent.putExtra("neighbourPhoneNumber",neighbour.getPhoneNumber());
+            intent.putExtra("neighbourAddress",neighbour.getAddress());
+            intent.putExtra("neighbourIsFavorite",neighbour.getIsFavorite());
+            mContext.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNeighbours.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.item_list_avatar)
+        public ImageView mNeighbourAvatar;
+        @BindView(R.id.item_list_name)
+        public TextView mNeighbourName;
+        @BindView(R.id.item_list_delete_button)
+        public ImageButton mDeleteButton;
+
+        ConstraintLayout parentLayout;
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+            parentLayout = view.findViewById(R.id.mon_layout);
+        }
+    }
+}
