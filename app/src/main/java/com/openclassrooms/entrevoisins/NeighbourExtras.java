fbfe7f838467b182ac90f064d76b3bfaf59bd51e
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-07 18:01:46 +0100

    commit NeighbourExtras, a class of constants

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/NeighbourExtras.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.entrevoisins;
+
+public class NeighbourExtras {
+    public static final String KEY_NEIGHBOUR_ID = "neighbourId";
+    public static final String KEY_NEIGHBOUR_NAME = "neighbourName";
+    public static final String KEY_NEIGHBOUR_AVATAR_URL = "neighbourAvatarUrl";
+    public static final String KEY_NEIGHBOUR_ABOUT_ME = "neighbourAboutMe";
+    public static final String KEY_NEIGHBOUR_ADDRESS = "neighbourAddress";
+    public static final String KEY_NEIGHBOUR_PHONE_NUMBER = "neighbourPhoneNumber";
+    public static final String KEY_NEIGHBOUR_IS_FAVORITE = "neighbourIsFavorite";
+    public static final String KEY_IS_FAVORITE = "IsFavorite";
+}
